d785c643817c9f8ac537dace0b2fbb4749d8fd63
Dragos Lup <dlup2003@gmail.com>
2024-02-26 01:20:16 -0500

    Added firebase database stuff

--- app/src/main/java/com/example/cs2340ateam34/CreateAccountView.java
+++ app/src/main/java/com/example/cs2340ateam34/CreateAccountView.java
@@ -1,37 +1,68 @@
 package com.example.cs2340ateam34;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.widget.EditText;
 import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.DatabaseReference;
+
 public class CreateAccountView extends AppCompatActivity {
     private EditText username;
     private EditText password;
-    public String answer;
-
+    private DatabaseReference mDatabase;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.create_account);
-        FirebaseDatabase database = FirebaseDatabase.getInstance();
+
         username = findViewById(R.id.usernameText);
         password = findViewById(R.id.passwordText);
         Button createAccountButton = findViewById(R.id.createAccountButton);
 
+        mDatabase = FirebaseDatabase.getInstance().getReference();
         createAccountButton.setOnClickListener(v -> {
-            DatabaseReference myRef = database.getReference(username.getText().toString());
 
+            String uname = username.getText().toString();
+            String upass = password.getText().toString();
+
+            //There should be code here that checks the database to see if uname is already in the database
+
+
+            if (!uname.isEmpty() && !uname.contains(" ")
+                && !upass.isEmpty() && !upass.contains(" ")) {
+                mDatabase.child("users").child(uname).setValue(upass);
+                Intent toCreateAccount = new Intent(CreateAccountView.this, MainActivity.class);
+                startActivity(toCreateAccount);
+            } else {
+                TextView t = findViewById(R.id.invalidCredentialsText);
+                t.setText("Username and Password cannot contain spaces or be null!");
+            }
+
+            //TextView t = findViewById(R.id.createAccountTitle);
+
+            //t.setText(mDatabase.child("users").child(username.getText().toString()).get()[0].getValue(password));
+            /*
+            mDatabase.child("users").child(uname).get().addOnCompleteListener(new OnCompleteListener<DataSnapshot>() {
+                @Override
+                public void onComplete(@NonNull Task<DataSnapshot> task) {
+                    if (!task.isSuccessful()) {
+                        Log.e("firebase", "Error getting data", task.getException());
+                    } else {
+                        //Log.d("firebase", String.valueOf(task.getResult().getValue()));
+                        x = String.valueOf(task.getResult().getValue());
+                    }
+                }
+            });
+
+            t.setText(x);
+            */
         });
 
-    }
-    public void databaseFunction() {
-        FirebaseDatabase data = FirebaseDatabase.getInstance();
-        String username = "dragos";
-        DatabaseReference myRef = data.getReference(username);
-        answer = myRef.toString();
+
     }
 }
--- app/src/main/java/com/example/cs2340ateam34/LogInView.java
+++ app/src/main/java/com/example/cs2340ateam34/LogInView.java
@@ -1,23 +1,66 @@
 package com.example.cs2340ateam34;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class LogInView extends AppCompatActivity {
 
     //should have an option for google authentication with firebase
     private EditText username;
     private EditText password;
-
+    private DatabaseReference mDatabase;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.create_account);
+        setContentView(R.layout.log_in);
 
         username = findViewById(R.id.usernameText);
         password = findViewById(R.id.passwordText);
+
+        Button loginButton = findViewById(R.id.loginButton);
+        mDatabase = FirebaseDatabase.getInstance().getReference();
+        loginButton.setOnClickListener(v -> {
+
+            String uname = username.getText().toString();
+            String upass = password.getText().toString();
+
+            //There should be code here that checks the database to see if uname is already in the database
+
+            if (1 == 1) { //IF THE USERNAME AND PASSWORD ARE CORRECT
+                Intent toCreateAccount = new Intent(LogInView.this, MainActivity.class);
+                startActivity(toCreateAccount);
+            } else {
+                TextView t = findViewById(R.id.invalidCredentialsText);
+                t.setText("Username and Password incorrect! Try creating account?");
+            }
+
+            //TextView t = findViewById(R.id.createAccountTitle);
+
+            //t.setText(mDatabase.child("users").child(username.getText().toString()).get()[0].getValue(password));
+            /*
+            mDatabase.child("users").child(uname).get().addOnCompleteListener(new OnCompleteListener<DataSnapshot>() {
+                @Override
+                public void onComplete(@NonNull Task<DataSnapshot> task) {
+                    if (!task.isSuccessful()) {
+                        Log.e("firebase", "Error getting data", task.getException());
+                    } else {
+                        //Log.d("firebase", String.valueOf(task.getResult().getValue()));
+                        x = String.valueOf(task.getResult().getValue());
+                    }
+                }
+            });
+
+            t.setText(x);
+            */
+        });
     }
 
 }

0ad0c4212603c3d9edfd37e814685de7fb75d9b8
AbhayVittal <avittal31@gatech.edu>
2024-02-25 22:16:25 -0500

    Finished WelcomeView

--- app/src/main/java/com/example/cs2340ateam34/WelcomeView.java
+++ app/src/main/java/com/example/cs2340ateam34/WelcomeView.java
@@ -1,24 +1,28 @@
 package com.example.cs2340ateam34;
 
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
 
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class WelcomeView extends AppCompatActivity {
     @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
+    protected void onCreate( Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.welcome_screen);
+        Button toLogInButton = findViewById(R.id.Log_In_Button);
+        Button toCreateAccountButton = findViewById(R.id.Create_Account_Button);
+
+        toLogInButton.setOnClickListener(v -> {
+            Intent toLogIn = new Intent(WelcomeView.this, LogInView.class);
+            startActivity(toLogIn);
+        });
+        toCreateAccountButton.setOnClickListener(v -> {
+            Intent toCreateAccount = new Intent(WelcomeView.this, CreateAccountView.class);
+            startActivity(toCreateAccount);
+        });
     }
 
-    /*
-    Make Log In Button
-     */
-
-    /*
-    Make Sign In Button
-     */
 }

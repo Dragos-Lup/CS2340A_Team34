0fd085e9da637d4fa781cbdb9c2fd64949171583
Dragos Lup <dlup2003@gmail.com>
2024-02-25 23:50:17 -0500

    finished

--- app/src/main/java/com/example/cs2340ateam34/HomeView.java
+++ app/src/main/java/com/example/cs2340ateam34/HomeView.java
@@ -7,9 +7,10 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-public class HomeView extends Fragment{
+public class HomeView extends Fragment {
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
+    public View onCreateView(LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
         return inflater.inflate(R.layout.home, container, false);
     }
 

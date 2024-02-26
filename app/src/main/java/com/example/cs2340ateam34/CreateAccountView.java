da59be7b4b1403f4aa099d375b0c7218027aa9a7
AbhayVittal <avittal31@gatech.edu>
2024-02-25 22:30:07 -0500

    Added UI references to login and createAccount

--- app/src/main/java/com/example/cs2340ateam34/CreateAccountView.java
+++ app/src/main/java/com/example/cs2340ateam34/CreateAccountView.java
@@ -1,6 +1,21 @@
 package com.example.cs2340ateam34;
 
+import android.os.Bundle;
+import android.widget.EditText;
+
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class CreateAccountView extends AppCompatActivity {
+    private EditText username;
+    private EditText password;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.create_account);
+
+        username = findViewById(R.id.usernameText);
+        password = findViewById(R.id.passwordText);
+    }
 }
--- app/src/main/java/com/example/cs2340ateam34/LogInView.java
+++ app/src/main/java/com/example/cs2340ateam34/LogInView.java
@@ -1,9 +1,23 @@
 package com.example.cs2340ateam34;
 
+import android.os.Bundle;
+import android.widget.EditText;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class LogInView extends AppCompatActivity {
 
     //should have an option for google authentication with firebase
+    private EditText username;
+    private EditText password;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.create_account);
+
+        username = findViewById(R.id.usernameText);
+        password = findViewById(R.id.passwordText);
+    }
 
 }

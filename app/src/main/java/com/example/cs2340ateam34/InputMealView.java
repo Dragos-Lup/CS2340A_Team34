4a1794b47185c78ac8077383bfacc71bc2aa68c3
Sophia Dumont <sophiadumont28@gmail.com>
2024-03-10 14:07:20 -0400

    Worked on input meal screen

--- app/src/main/java/com/example/cs2340ateam34/InputMealView.java
+++ app/src/main/java/com/example/cs2340ateam34/InputMealView.java
@@ -5,11 +5,22 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class InputMealView extends Fragment {
+    private EditText meal;
+
+    private EditText calories;
+
+    private DatabaseReference meals;
     @Override
     public View onCreateView(LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.ingredients_screen, container, false);
+        return inflater.inflate(R.layout.input_meal, container, false);
     }
 
 }

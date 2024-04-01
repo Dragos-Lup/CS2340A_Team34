5d3c849a2970012c43371a8d0bd87c983218db99
AbhayVittal <avittal31@gatech.edu>
2024-03-31 23:53:37 -0400

    Made a recipe error message

--- app/src/main/java/com/example/cs2340ateam34/RecipeView.java
+++ app/src/main/java/com/example/cs2340ateam34/RecipeView.java
@@ -7,6 +7,7 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.RadioButton;
+import android.widget.TextView;
 
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
@@ -19,6 +20,8 @@ public class RecipeView extends Fragment {
     EditText recipeName;
     EditText items;
     Button enter;
+
+    TextView recipeError;
 
     RadioButton filterAll;
     RadioButton filterMakeable;
@@ -38,6 +41,7 @@ public class RecipeView extends Fragment {
         recipeName = view.findViewById(R.id.recipe_name);
         items = view.findViewById(R.id.recipeitems);
         enter = view.findViewById(R.id.recipeenter);
+        recipeError = view.findViewById(R.id.recipe_error);
         filterAll = view.findViewById(R.id.filter_all);
         filterMakeable = view.findViewById(R.id.filter_makeable);
         filterNotMakeable = view.findViewById(R.id.filter_notMakeable);
@@ -50,18 +54,29 @@ public class RecipeView extends Fragment {
             ArrayList<RecipeItem> recipeItems = new ArrayList<>();
             String itemsraw = items.getText().toString();
             String[] itemssep = itemsraw.split(",");
+            boolean quantityError = false;
             for (String itemraw : itemssep) {
                 String[] item = itemraw.split("-");
                 String itemname = item[0].trim();
                 int itemquantity = Integer.parseInt(item[1].trim());
+                if (itemquantity < 1) {
+                    quantityError = true;
+                    break;
+                }
                 RecipeItem recipeItem = new RecipeItem(itemname, itemquantity);
                 recipeItems.add(recipeItem);
             }
-            Recipe recipe = new Recipe(name, recipeItems);
-            user.addRecipe(recipe);
+            if (quantityError) {
+                recipeError.setText("Input quantity cannot be less than 1.");
+            } else {
+                Recipe recipe = new Recipe(name, recipeItems);
+                user.addRecipe(recipe);
+                recipeError.setText("");
+                recipeName.setText("");
+                items.setText("");
+            }
+
             setRecyclerView();
-            recipeName.setText("");
-            items.setText("");
         });
 
         filterAll.setOnCheckedChangeListener((buttonView, isChecked) -> {
